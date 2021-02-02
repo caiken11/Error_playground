9b905e3eedaa56f1c2b40fb16304dd37d31c7faa
Crystal Aiken <caiken2011@gmail.com>
2021-02-01 20:44:55 -0500

    updated README

---
+++ src/typesVarsBranching/Main.java
@@ -0,0 +1,17 @@
+package typesVarsBranching;
+
+public class Main {
+
+//	double num2;
+	public static void main(String[] args) {
+//		int 1num = 2;
+		
+//		String my Name = "my name is Crystal";
+		
+//		boolean yes;
+//		if(yes) {
+//			System.out.println("Crystal");
+//	}
+//		System.out.println(num2);
+	}
+}
